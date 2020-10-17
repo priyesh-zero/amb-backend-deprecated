59cb96788b6e52f5c3da11e95929dbe0f1612bd4
Eagle Tech <eagletech.apst@gmail.com>
2020-10-17 02:27:13 +0000

    server.ts edited online with Bitbucket adding https://ambulancia.netlify.app/login url for the newly created application

--- server.ts
+++ server.ts
@@ -21,7 +21,8 @@ class Server {
             "https://ambulancia-backend.herokuapp.com",
             "http://localhost:4200",
             "http://127.0.0.1:3000",
-            "http://127.0.0.1:4000"
+            "http://127.0.0.1:4000",
+            "https://ambulancia.netlify.app"
         ];
     }
 
